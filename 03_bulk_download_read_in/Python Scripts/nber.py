e23819bd08150d6f3df8896e0008ef7c0696d05b
ckjohnson7 <cecilejohnson01@gmail.com>
2020-06-15 10:46:16 -0400

    update python read-in files

--- 03_bulk_download_read_in/Python Scripts/nber.py
+++ 03_bulk_download_read_in/Python Scripts/nber.py
@@ -4,17 +4,19 @@
 import os
 import zipfile as zip
 import pandas as pd
+import csv
+import numpy as np
 
 # Set up file path:
 # Please include the folder path of the file you are reading. Ex: os.chdir("C:/Users/johnsmith/Downloads")
-os.chdir("//dc1fs/dc1ehd/share/Science Policy Portfolio/PatentsView IV/Documentation/Tables/20200331")
+os.chdir("")
 
 file_name = "nber.tsv.zip"
 f_name = "nber.tsv"
 # Selecting the zip file.
 zf = zip.ZipFile(file_name)
 # Reading the selected file in the zip.
-df = pd.read_csv(zf.open(f_name), delimiter="\t")
+df = pd.read_csv(zf.open(f_name), delimiter="\t", quoting = csv.QUOTE_NONNUMERIC)
 
 # Print first five observations
 df.head()
@@ -22,4 +24,4 @@ df.head()
 print(len(df))
 df.info()
 # Print basic summary statistics for numerical variables
-print(df.describe(df.select_dtypes(include='int')))
+print(df.describe(exclude=[np.number]))
--- 03_bulk_download_read_in/Python Scripts/otherreference.py
+++ 03_bulk_download_read_in/Python Scripts/otherreference.py
@@ -4,17 +4,19 @@
 import os
 import zipfile as zip
 import pandas as pd
+import csv
+import numpy as np
 
 # Set up file path:
 # Please include the folder path of the file you are reading. Ex: os.chdir("C:/Users/johnsmith/Downloads")
-os.chdir("//dc1fs/dc1ehd/share/Science Policy Portfolio/PatentsView IV/Documentation/Tables/20200331")
+os.chdir("")
 
 file_name = "otherreference.tsv.zip"
 f_name = "otherreference.tsv"
 # Selecting the zip file.
 zf = zip.ZipFile(file_name)
 # Reading the selected file in the zip.
-df = pd.read_csv(zf.open(f_name), delimiter="\t")
+df = pd.read_csv(zf.open(f_name), delimiter="\t", quoting = csv.QUOTE_NONNUMERIC)
 
 chunksize = 15*(10 ** 5)
 count = 1
@@ -27,3 +29,4 @@ for df in pd.read_csv(zf.open(f_name), delimiter="\t", chunksize=chunksize):
 # Print summary of data: number of observations, columns, and each variable data type
 print(n_obs)
 print(df.dtypes)
+print(df.describe(exclude=[np.number]))
