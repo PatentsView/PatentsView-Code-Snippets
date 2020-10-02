91d6c528513adf2d8f198b7fcd3a5532ff8fe7f7
TutorJoanne <jtutor@air.org>
2020-10-02 17:41:47 -0400

    Added pre-grant read-in scripts

---
+++ 04_bulk_pregrant_read_in/Python Scripts/claim_2018.py
@@ -0,0 +1,31 @@
+#Read-in script for 2018 Claims Data
+
+# Importing necessary packages.
+import os
+import zipfile as zip
+import pandas as pd
+import csv
+import numpy as np
+# Set up file path:
+# Please include the folder path of the file you are reading. Ex: os.chdir("C:/Users/johnsmith/Downloads")
+os.chdir("")
+
+file_name = "claim_2018.tsv.zip"
+f_name = "claim_2018.tsv"
+# Selecting the zip file.
+zf = zip.ZipFile(file_name)
+# Reading the selected file in the zip.
+chunksize = 10 ** 4
+count = 1
+n_obs = 0
+final = []
+for df in pd.read_csv(zf.open(f_name), delimiter="\t", chunksize=chunksize, quoting=csv.QUOTE_NONNUMERIC):
+    print('processing chunk: ' + str(count))
+    n_obs += len(df)
+    count += 1
+    final.append(df)
+# Create data frame with all observations
+df = pd.concat(final)
+# Print summary of data: number of observations, columns, and each variable data type
+print(n_obs)
+print(df.dtypes)
