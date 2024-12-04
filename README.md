c05e082507096aba7b771d80e96cd377af451007
Olivier Binette <olivier.binette@gmail.com>
2024-12-04 10:27:22 -0500

    Add patentsearch example; gitignore

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # PatentsView Code Examples
 
-Examples for working with [PatentsView's bulk data downloads](https://patentsview.org/download/data-download-tables) and [PatentsView's PatentSearch API](https://search.patentsview.org/docs/2024/11/06/2.2-release).
+Examples working with [PatentsView's bulk data downloads](https://patentsview.org/download/data-download-tables) and [PatentsView's PatentSearch API](https://search.patentsview.org/docs/2024/11/06/2.2-release).
 
 ## Examples List
 
 ### Bulk Data Downloads
-- Basic tips and tricks
-- Loading granted and pre-grant patent data tables
-  - Python examples
-  - R examples
+- Getting Started with PatentsView downloads
+- 
 
 ### PatentSearch API
 - PatentSearch API Demo
--- tests/test_data_downloads.py
+++ tests/test_data_downloads.py
@@ -1,6 +1,9 @@
+import yaml
+import subprocess
 import pytest
 from testbook import testbook
 
+# Change the working directory to the Data Downloads folder for all tests in this file.
 @pytest.fixture(autouse=True)
 def change_test_dir(monkeypatch):
     monkeypatch.chdir('Data Downloads')
@@ -9,3 +12,18 @@ def change_test_dir(monkeypatch):
 @testbook('Data Downloads/0-introduction.ipynb', execute=True)
 def test_0_introduction(nb):
     ...  # Just check that notebook runs without error.
+
+
+def test_sources():
+    with open("sources.yml") as file:
+        sources = yaml.safe_load(file)
+    
+    for data in [sources['granted'], sources['pre-grant']]:
+        for source in data.values():
+            database = source['database']
+            url_template = source['url_template']
+            for table in source['tables']:
+                table_url = url_template.format(database=database, table=table)
+                
+                print("Checking ", table_url)
+                subprocess.run(['curl', '--head', '--fail', table_url], check=True)
