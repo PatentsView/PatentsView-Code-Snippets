c5d1abcbacdd8e905384f6919472a183de0e3581
Olivier Binette <olivier.binette@gmail.com>
2024-12-11 08:24:05 -0500

    Documentation improvements

--- tests/test_data_downloads.py
+++ tests/test_data_downloads.py
@@ -11,19 +11,23 @@ def change_test_dir(monkeypatch):
 
 @testbook('data-downloads/0-getting-started.ipynb', execute=True)
 def test_0_introduction(nb):
-    ...  # Just check that notebook runs without error.
+    """Check that notebook runs without error."""
+    ...
 
 
 def test_sources():
+    """Check that all urls in sources.yml are valid."""
     with open("sources.yml") as file:
         sources = yaml.safe_load(file)
 
     for data in [sources['granted'], sources['pre-grant']]:
         for source in data.values():
+            # E.g. database = 'https://s3.amazonaws.com/data.patentsview.org/download'
             database = source['database']
             url_template = source['url_template']
             for table in source['tables']:
+                # E.g. table_url = https://s3.amazonaws.com/data.patentsview.org/download/g_patent.tsv.zip
                 table_url = url_template.format(database=database, table=table)
                 
-                print("Checking ", table_url)
+                print('Checking ', table_url)
                 subprocess.run(['curl', '--head', '--fail', table_url], check=True)
